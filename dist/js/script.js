a6b8b8b94ee679553fa5e333fe4974338f0fd2f4
Anastasia <nastia_cool55@gmail.com>
2020-12-21 16:28:11 +0300

    added script for timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -91,13 +91,22 @@
   !*** ./src/js/main.js ***!
   \************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
 
 window.addEventListener('DOMContentLoaded', () => {
   //variables
   const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParent = document.querySelector('.tabheader__items'); //functions
+        tabsParent = document.querySelector('.tabheader__items'),
+        deadline = '2020-12-31',
+        millisecondsInASecond = 1000,
+        millisecondsInADay = millisecondsInASecond * 60 * 60 * 24,
+        millisecondsInAnHour = millisecondsInASecond * 60 * 60,
+        millisecondsInAMinute = millisecondsInASecond * 60; //Tabs
+  //functions for tabs
 
   const hideTabContent = () => {
     tabsContent.forEach(tabContent => {
@@ -130,7 +139,62 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       });
     }
-  });
+  }); //Timer
+  //functions for timer
+
+  const getTimeRemaining = endtime => {
+    //gets time from now to the endtime
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+          //in ms
+    days = Math.floor(t / millisecondsInADay),
+          //days in t
+    hours = Math.floor(t / millisecondsInAnHour % 24),
+          minutes = Math.floor(t / millisecondsInAMinute % 60),
+          seconds = Math.floor(t / millisecondsInASecond % 60);
+    return {
+      'totalTimeRemaining': t,
+      'daysRemaining': days,
+      'hoursRemaining': hours,
+      'minutesRemaining': minutes,
+      'secondsRemaining': seconds
+    };
+  };
+
+  const putZero = num => {
+    //returns small numbers with zero in front
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  };
+
+  const setTimer = (timerSelector, endtime) => {
+    const timer = document.querySelector(timerSelector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector("#minutes"),
+          seconds = timer.querySelector('#seconds'),
+          //updates timer each second
+    timeInterval = setInterval(updateTimer, 1000);
+    updateTimer();
+
+    function updateTimer() {
+      const t = getTimeRemaining(endtime); //puts numbers to HTML
+
+      days.innerHTML = putZero(t.daysRemaining);
+      hours.innerHTML = putZero(t.hoursRemaining);
+      minutes.innerHTML = putZero(t.minutesRemaining);
+      seconds.innerHTML = putZero(t.secondsRemaining);
+
+      if (t.totalTimeRemaining <= 0) {
+        //stops updating timer when endtime comes
+        clearInterval(timeInterval);
+      }
+    }
+  };
+
+  setTimer(".timer", deadline);
 });
 
 /***/ })
