d92c77645cd49dce2e8d878787bdeda09d74f34f
Anastasia <nastiacool55@gmail.com>
2021-01-19 16:08:31 +0300

    developed menu items with JS Classes

--- src/js/main.js
+++ src/js/main.js
@@ -147,4 +147,67 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     window.addEventListener('scroll', openModalByScroll);
+
+    //Classes for menu items
+
+    class MenuItem {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 75;
+            this.changeToRUB();
+        }
+
+        changeToRUB() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() { //метод для создания верстки экземпляра класса
+            const menuItemElement = document.createElement('div');
+            menuItemElement.innerHTML = `
+                <div class="menu__item">
+                        <img src=${this.src} alt=${this.alt}>
+                        <h3 class="menu__item-subtitle">${this.title}</h3>
+                        <div class="menu__item-descr">${this.descr}</div>
+                        <div class="menu__item-divider"></div>
+                        <div class="menu__item-price">
+                            <div class="menu__item-cost">Цена:</div>
+                            <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+                        </div>
+                </div>
+            `;
+            this.parent.append(menuItemElement); //добавляем верстку внутрь родителя
+        }
+    }
+
+    new MenuItem(
+        "img/tabs/vegy.jpg", 
+        "vegy", 
+        'Меню "Фитнес"', 
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 
+        4, 
+        ".menu__field > .container"
+    ).render();
+
+    new MenuItem(
+        "img/tabs/elite.jpg", 
+        "elite", 
+        'Меню "Премиум"', 
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 
+        8, 
+        ".menu__field > .container"
+    ).render();
+
+    new MenuItem(
+        "img/tabs/post.jpg", 
+        "post", 
+        'Меню "Постное"', 
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, растительное молоко, правильное количество белков за счет тофу и вегетарианских стейков.', 
+        6, 
+        ".menu__field > .container"
+    ).render();
 });
