ba191cdbc5b94ed8a76aad8e9f7754d1d44f5e0c
Anastasia <nastiacool55@gmail.com>
2021-01-19 17:00:12 +0300

    Modified script to add CSS-classes with help of JS-class

--- dist/js/script.js
+++ dist/js/script.js
@@ -241,12 +241,14 @@ window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('scroll', openModalByScroll); //Classes for menu items
 
   class MenuItem {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes; //через Rest-оператор, чтобы в будущем передавать неограниченное число классов
+
       this.parent = document.querySelector(parentSelector);
       this.transfer = 75;
       this.changeToRUB();
@@ -259,17 +261,24 @@ window.addEventListener('DOMContentLoaded', () => {
     render() {
       //метод для создания верстки экземпляра класса
       const menuItemElement = document.createElement('div');
+
+      if (this.classes.length === 0) {
+        //this.classes=[] (из-за Rest-оператора), если ничего не передано
+        this.classes = 'menu__item';
+        menuItemElement.classList.add(this.classes); //добавляем CSS-класс по умолчанию, если через JS-класс они не будут переданы
+      } else {
+        this.classes.forEach(className => menuItemElement.classList.add(className)); //добавляем переданные через JS-класс CSS-классы элементу
+      }
+
       menuItemElement.innerHTML = `
-                <div class="menu__item">
-                        <img src=${this.src} alt=${this.alt}>
-                        <h3 class="menu__item-subtitle">${this.title}</h3>
-                        <div class="menu__item-descr">${this.descr}</div>
-                        <div class="menu__item-divider"></div>
-                        <div class="menu__item-price">
-                            <div class="menu__item-cost">Цена:</div>
-                            <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
-                        </div>
-                </div>
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+                    </div>
             `;
       this.parent.append(menuItemElement); //добавляем верстку внутрь родителя
     }
